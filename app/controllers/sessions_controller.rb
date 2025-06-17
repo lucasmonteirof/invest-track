2c31ae1dd01635485ef5e2d7e8465967987fbeef
Lucas Monteiro <lucasmonteiroferreira1@gmail.com>
2025-06-17 17:12:55 -0300

    Implement user login flow

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+    render :new, status: :ok
+  end
+
+  def create
+    user = User.find_by(login: params[:login])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      head :ok
+    else
+      head :unauthorized
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root to: "sessions#new"
+
   resources :users, only: [ :new, :create ]
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
